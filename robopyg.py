025956fab40e2cb9d8b5f9c0b9a2d86a15a03141
David Barnett <davidbarnett2@gmail.com>
2010-07-15 13:39:17 -0400

    Created SyntaxInstance class to encapsulate decoding syntax string

--- robopyg.py
+++ robopyg.py
@@ -2,6 +2,28 @@ import glob
 import os
 import sys
 
+class SyntaxInstance(object):
+    @classmethod
+    def from_special_string(cls, syntax_string):
+        spans = []
+        syntax_string_lines = syntax_string.split('\n')
+        it = iter(syntax_string_lines)
+        for line in it:
+            if line == '---':
+                break
+            start, end, tokencode = line.split(' ')
+            start_col, start_row = map(int, start.split(','))
+            end_col, end_row = map(int, end.split(','))
+            spans.append(((start_col, start_row),
+                        (end_col, end_row),
+                        int(tokencode)))
+        syntax_text = '\n'.join(list(it))
+        return cls(syntax_text, spans)
+
+    def __init__(self, text, spans):
+        self.text = text
+        self.spans = spans
+
 def main(argv=None):
     if argv is None:
         argv = sys.argv
@@ -13,19 +35,8 @@ def main(argv=None):
         _, syntax_name = os.path.split(path)
         with open(fname, 'r') as f:
             format_spec = formats.setdefault(syntax_name, [])
-            spans = []
-            for line in f:
-                line = line.rstrip('\n')
-                if line == '---':
-                    break
-                start, end, tokencode = line.split(' ')
-                start_col, start_row = map(int, start.split(','))
-                end_col, end_row = map(int, end.split(','))
-                spans.append(((start_col, start_row),
-                            (end_col, end_row),
-                            int(tokencode)))
-            syntax_text = ''.join(f)
-            format_spec.append({'spans': spans, 'text': syntax_text})
+            syntax_inst = SyntaxInstance.from_special_string(f.read())
+            format_spec.append(syntax_inst)
     return 0
 
 if __name__ == '__main__':

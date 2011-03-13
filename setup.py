30ad80d2d082a25950c9e4c533ceec06cf5926d1
David Barnett <davidbarnett2@gmail.com>
2011-03-13 11:44:41 -0400

    Added setup.py, with colorama dependency

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "cyberpyg",
+    version = "0.09",
+    packages=find_packages(),
+    dependency_links = [],
+    install_requires=['colorama'],
+    extras_require={},
+    package_data = {},
+    author="David Barnett",
+    author_email = "davidbarnett2@gmail.com",
+    description = "syntax highlighting / lexing framework",
+    license = "BSD",
+    keywords= "",
+    url = "http://github.com/dbarnett/cyberpyg",
+    entry_points = {
+        "console_scripts": [
+        ]
+    }
+)
